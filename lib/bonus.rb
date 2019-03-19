a77de6435ff05ffad713e214d9f5105e8e306fda
Emilia Ziemichód <emifrompoland@gmail.com>
2019-03-19 06:58:39 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -24,4 +24,4 @@ def bonus
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
 end
-epic_tragedy["Romeo"][status:]<<"dead"
+epic_tragedy["Romeo"][status:]="dead"
